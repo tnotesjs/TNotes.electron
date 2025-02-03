1d0c788f4469d56d271f8f117a981e86b9c1b62f
Tdahuyou <12875288+tdahuyou@user.noreply.gitee.com>
2025-02-03 18:22:44 +0800

    update: 1 files modified

--- 0002. 使用 contextBridge 暴露 API 给渲染进程/demos/1/preload.js
+++ 0002. 使用 contextBridge 暴露 API 给渲染进程/demos/1/preload.js
@@ -12,3 +12,15 @@ if (process.contextIsolated) {
 } else {
   window.TdahuyouPlugin = TdahuyouAPI
 }
+
+/* 
+if (process.contextIsolated) {
+  contextBridge.exposeInMainWorld('TdahuyouPlugin', TdahuyouAPI)
+} else {
+  window.TdahuyouPlugin = TdahuyouAPI
+}
+
+这部分代码最终会将 TdahuyouAPI 暴露给渲染进程
+相当于执行了 window.TdahuyouPlugin = TdahuyouAPI
+可以在渲染进程中通过 window.TdahuyouPlugin 调用我们封装的 TdahuyouAPI
+*/
